47ce947479886731aaae317b9a8035d4190e9509
Branden Simbeck <branden@brandensimbeck.com>
2017-05-12 20:46:42 -0400

    Adding Models

--- Assignment Submission Tool/Assignment Submission Tool.Model/Assignment.cs
+++ Assignment Submission Tool/Assignment Submission Tool.Model/Assignment.cs
@@ -6,7 +6,11 @@ using System.Threading.Tasks;
 
 namespace Assignment_Submission_Tool.Model
 {
-    public class Class1
+    public class Assignment
     {
+        public int AssignmentID { get; set; }
+        public string AssignmentLanguage { get; set; }
+        public string AssignmentDescription { get; set; }
+        public List<CodeEval> Tests { get; set; }
     }
 }
--- Assignment Submission Tool/Assignment Submission Tool.Model/AssignmentSubmission.cs
+++ Assignment Submission Tool/Assignment Submission Tool.Model/AssignmentSubmission.cs
@@ -8,5 +8,9 @@ namespace Assignment_Submission_Tool.Model
 {
     class AssignmentSubmission
     {
+        public int AssignmentSubmissionID { get; set; }
+        public Assignment AssignmentInformation { get; set; }
+        public string AssignmentFileName { get; set; }
+        public string AssignmentFileContents { get; set; }
     }
 }
